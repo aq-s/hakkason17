058cf4a3794b34df26049c46d6364dd10c22e555
pengin-star <cocapple@outlook.jp>
2025-02-16 13:09:56 +0900

    javascript update

--- application.py
+++ application.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def main_page():
     return render_template("index.html")
 
+@app.route("/home", methods=["GET","POST"])
+def home_page():
+    return render_template("home.html")
+
 ## 実行
 if __name__ == "__main__":
     app.run(debug=True)
